b9318fe4c3f0c3283aa3de469e26610eec059e13
chxrlxy <charleydibley21@gmail.com>
2026-07-09 16:24:40 +0100

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        var solutionArray = new int[2];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    solutionArray[0] = i;
+                    solutionArray[1] = j;
+
+                    Array.Sort(solutionArray);
+
+                    return solutionArray;
+                }
+            }
+        }
+
+        return solutionArray;
+    }
+}
